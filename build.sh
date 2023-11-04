565f85316df1aaa73638fe7a6b7899d52959f504
root <root@vmi1503662.contaboserver.net>
2023-11-04 08:08:50 +0800

    fixing

--- build.sh
+++ build.sh
@@ -16,15 +16,15 @@ docker build -t strongpapazola/hadoop-submit:3.3.4-java8 submit/.
 # How To Run Using docker-compose
 
 # Migrate Data From SQL To Hadoop (Deprecated!!)
-docker build -t strongpapazola/hadoop-sqoop:3.3.4-java8 sqoop/.
+#docker build -t strongpapazola/hadoop-sqoop:3.3.4-java8 sqoop/.
 # USINGCOMPOSEEE docker run -it --rm --name sqoop strongpapazola/hadoop-sqoop:3.3.4-java8 
 
-docker build -t strongpapazola/hadoop-flume:3.3.4-java8 flume/.
+#docker build -t strongpapazola/hadoop-flume:3.3.4-java8 flume/.
 # USINGCOMPOSEEE docker run -it --rm --name flume --network development --hostname flume -e SERVICE_PRECONDITION="117.53.45.158:9000 117.53.45.158:9870 117.53.45.158:9864 117.53.45.158:8088" --env-file ./hadoop.env strongpapazola/hadoop-flume:3.3.4-java8 bash
 # flume-ng agent --conf conf --conf-file /opt/flume/conf/flume.conf --name agent -Dflume.root.logger=INFO,console
 
 
-docker build -t strongpapazola/hadoop-zookeeper:3.3.4-java8 zookeeper/.
+#docker build -t strongpapazola/hadoop-zookeeper:3.3.4-java8 zookeeper/.
 # USINGCOMPOSEEE docker run -it --rm --name zookeeper strongpapazola/hadoop-zookeeper:3.3.4-java8 
 
 
